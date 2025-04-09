f21950e35de73a50120dbc4e37639aad1595dffd
streqmin <sanghoo1023@gmail.com>
2025-04-09 15:16:04 +0900

    init: 기본 사용자 모델을 account 앱의 User 모델로 설정

--- config/settings.py
+++ config/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
+    "accounts",
 ]
 
 MIDDLEWARE = [
@@ -74,16 +75,18 @@ WSGI_APPLICATION = "config.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': "conveting",
-        'USER': "conveting",
-        'PASSWORD': "Conveting1!",
-        'HOST': 'localhost',
-        'PORT': "3306"
+    "default": {
+        "ENGINE": "django.db.backends.mysql",
+        "NAME": "conveting",
+        "USER": "conveting",
+        "PASSWORD": "Conveting1!",
+        "HOST": "localhost",
+        "PORT": "3306",
     }
 }
 
+
+AUTH_USER_MODEL = "accounts.User"
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
