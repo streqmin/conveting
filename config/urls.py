15afecf6f0fb3925e6d657bd672d4f4fd913bc04
streqmin <sanghoo1023@gmail.com>
2025-04-12 17:11:37 +0900

    feat: allauth url 경로 설정

--- config/urls.py
+++ config/urls.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 
-urlpatterns = [path("admin/", admin.site.urls), path("api/auth/", include("accounts.urls"))]
+urlpatterns = [
+    path("admin/", admin.site.urls),
+    path("api/auth/", include("accounts.urls")),
+    path("accounts/", include("allauth.urls")),
+]
