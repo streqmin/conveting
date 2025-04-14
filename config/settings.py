94969f42c690bff1c4301d8e0ac6ef7aeeedf8d7
streqmin <sanghoo1023@gmail.com>
2025-04-14 11:24:09 +0900

    init: 커스텀 소셜 계정 어댑터 등록

--- config/settings.py
+++ config/settings.py
@@ -152,6 +152,8 @@ ACCOUNT_AUTHENTICATION_METHOD = "email"
 ACCOUNT_EMAIL_VERIFICATION = "none"  # 개발 중에는 'none'
 SOCIALACCOUNT_QUERY_EMAIL = True
 
+SOCIALACCOUNT_ADAPTER = "accounts.adapters.CustomSocialAccountAdapter"
+
 
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
