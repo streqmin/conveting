28de6d7b926e64ddfc94836407e2715419a0fb63
streqmin <sanghoo1023@gmail.com>
2025-04-15 15:49:25 +0900

    feat: 커스텀  카카오 어댑터가 allauth 의  oauth 어댑터를 상속하도록 변경

--- accounts/kakao_adapter.py
+++ accounts/kakao_adapter.py
@@ -1,16 +1,11 @@
-from allauth.socialaccount.providers.oauth2.views import OAuth2Adapter
+from allauth.socialaccount.providers.kakao.views import KakaoOAuth2Adapter
 from allauth.socialaccount.adapter import get_adapter
 from allauth.socialaccount.providers.kakao.provider import KakaoProvider
 
 
-class CustomKakaoOAuth2Adapter(OAuth2Adapter):
-    provider_id = KakaoProvider.id
-    access_token_url = "https://kauth.kakao.com/oauth/token"
-    authorize_url = "https://kauth.kakao.com/oauth/authorize"
-    profile_url = "https://kapi.kakao.com/v2/user/me"
-
+class CustomKakaoOAuth2Adapter(KakaoOAuth2Adapter):
     def complete_login(self, request, app, token, **kwargs):
-        access_token = token["access_token"]  # token은 dict 타입
+        access_token = token["access_token"]
         headers = {
             "Authorization": f"Bearer {access_token}",
             "Content-Type": "application/x-www-form-urlencoded;charset=utf-8",
