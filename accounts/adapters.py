e202a02c7dfe4b5c2ed72738bf130cc052beba41
streqmin <sanghoo1023@gmail.com>
2025-04-15 17:19:15 +0900

    chore: 예외처리 구문 수정

--- accounts/adapters.py
+++ accounts/adapters.py
@@ -7,25 +7,18 @@ User = get_user_model()
 
 class CustomSocialAccountAdapter(DefaultSocialAccountAdapter):
     def save_user(self, request, sociallogin: SocialLogin, form=None):
-        user = sociallogin.user
-
         provider = sociallogin.account.provider
         uid = sociallogin.account.uid
-
         extra_data = sociallogin.account.extra_data
-        nickname = extra_data.get("nickname") or extra_data.get("name") or "소셜사용자"
 
         try:
             return User.objects.get(social_type=provider, social_id=uid)
         except User.DoesNotExist:
-            pass
-
-        user = User.objects.create_social_user(
-            username=nickname,
-            social_type=provider,
-            social_id=uid,
-            email=extra_data.get("email", None),
-            profile_image=extra_data.get("picture", None),
-        )
-        sociallogin.user = user
-        return user
+            nickname = extra_data.get("nickname") or extra_data.get("name") or "소셜사용자"
+            return User.objects.create_social_user(
+                username=nickname,
+                social_type=provider,
+                social_id=uid,
+                email=extra_data.get("email"),
+                profile_image=extra_data.get("picture"),
+            )
