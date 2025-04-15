551d6bfbeb7a860a0bf9a96e6bf64c514a253f58
streqmin <sanghoo1023@gmail.com>
2025-04-15 13:29:50 +0900

    feat: 소셜 로그인 콜백 뷰 url 경로 추가

--- config/urls.py
+++ config/urls.py
@@ -1,8 +1,34 @@
 from django.contrib import admin
 from django.urls import path, include
+from accounts.views import SocialLoginCallbackView, index
+from accounts.kakao_adapter import CustomKakaoOAuth2Adapter
+from allauth.socialaccount.providers.google.views import GoogleOAuth2Adapter
+
 
 urlpatterns = [
     path("admin/", admin.site.urls),
+    path("", index),
     path("api/auth/", include("accounts.urls")),
+    path(
+        "accounts/social/login/callback/", SocialLoginCallbackView.as_view(), name="social_callback"
+    ),
+    path(
+        "accounts/kakao/login/callback/",
+        type(
+            "KakaoJWTCallbackView",
+            (SocialLoginCallbackView,),
+            {"adapter_class": CustomKakaoOAuth2Adapter},
+        ).as_view(),
+        name="kakao_jwt_callback",
+    ),
+    path(
+        "accounts/google/login/callback/",
+        type(
+            "GoogleJWTCallbackView",
+            (SocialLoginCallbackView,),
+            {"adapter_class": GoogleOAuth2Adapter},
+        ).as_view(),
+        name="google_jwt_callback",
+    ),
     path("accounts/", include("allauth.urls")),
 ]
