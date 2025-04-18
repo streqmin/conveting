43ee65f863b444de0a68dc980dee28d0728de5ef
streqmin <sanghoo1023@gmail.com>
2025-04-18 13:01:07 +0900

    feat: 반려견 모델 admin 페이지 등록

--- config/urls.py
+++ config/urls.py
@@ -1,9 +1,9 @@
-from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from accounts.views import index, AdminJWTLoginView
 from accounts.admin_site import jwt_admin_site
+import dogs.admin_site
 
 
 urlpatterns = [
