74a12e87370054882a4c09711bd42e757f9c3b04
streqmin <sanghoo1023@gmail.com>
2025-04-20 13:01:53 +0900

    feat: 질환 정보를 저장하는 model 생성

--- prediction/models.py
+++ prediction/models.py
@@ -1,1 +1,11 @@
 from django.db import models
+
+
+class DiseaseInfo(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.TextField(max_length=500)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        verbose_name = "질환 정보"
+        verbose_name_plural = "질환 정보"
