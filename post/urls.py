99f9b95707d57050049a63cd3c37e764247198da
streqmin <sanghoo1023@gmail.com>
2025-04-22 11:01:44 +0900

    feat: 댓글 생성을 위한 CommentCreateView 와 url 생성

--- post/urls.py
+++ post/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PostCreateView, PostListView, PostDetailView, toggle_like
+from .views import PostCreateView, PostListView, PostDetailView, toggle_like, CommentCreateView
 
 urlpatterns = [
     path("", PostListView.as_view(), name="post_list"),
     path("<int:pk>/", PostDetailView.as_view(), name="post_detail"),
     path("<int:post_id>/toggle-like/", toggle_like, name="toggle_like"),
+    path("<int:post_id>/comment/", CommentCreateView.as_view(), name="comment_create"),
     path("create/", PostCreateView.as_view(), name="post_create"),
 ]
