32752ae1679b4cc70b789400a68923b920fdc2fd
streqmin <sanghoo1023@gmail.com>
2025-04-22 15:22:43 +0900

    feat: 게시글 조회 view 를 로그인 필요로 수정 및 좋아요 view 를 cbv 로 수정

--- post/views.py
+++ post/views.py
@@ -4,11 +4,12 @@ from django.db.models import Q
 from django.http import JsonResponse, HttpResponseBadRequest
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
+from django.views import View
 from django.views.generic.edit import CreateView
 from django.views.generic import ListView, DetailView
 
 
-from .models import Post, PostLike, Comment
+from .models import Post, PostLike, Comment, CommentLike
 from .forms import PostForm, CommentForm
 
 
@@ -87,7 +88,7 @@ class PostListView(ListView):
         return context
 
 
-class PostDetailView(DetailView):
+class PostDetailView(LoginRequiredMixin, DetailView):
     model = Post
     template_name = "post/post_detail.html"
     context_object_name = "post"
@@ -96,37 +97,39 @@ class PostDetailView(DetailView):
         context = super().get_context_data(**kwargs)
         post = self.object
         user = self.request.user
-        context["has_liked"] = (
-            user.is_authenticated and PostLike.objects.filter(user=user, post=post).exists()
-        )
+
+        context["has_liked"] = PostLike.objects.filter(user=user, post=post).exists()
+
+        context["liked_comment_ids"] = CommentLike.objects.filter(
+            user=user, comment__post=post
+        ).values_list("comment_id", flat=True)
+
         context["comment_form"] = CommentForm()
         return context
 
 
-@login_required
-def toggle_like(request, post_id):
-    if request.method != "POST":
-        return HttpResponseBadRequest("잘못된 요청입니다.")
+class PostLikeToggleView(LoginRequiredMixin, View):
+    def post(self, request, post_id):
+        if request.method != "POST":
+            return HttpResponseBadRequest("잘못된 요청입니다.")
 
-    post = get_object_or_404(Post, id=post_id)
-    user = request.user
+        post = get_object_or_404(Post, id=post_id)
+        user = request.user
 
-    # PostLike를 가져오거나 없으면 생성
-    like, created = PostLike.objects.get_or_create(user=user, post=post)
+        like, created = PostLike.objects.get_or_create(user=user, post=post)
 
-    if not created:
-        # 이미 눌렀던 경우: 삭제
-        like.delete()
-        liked = False
-    else:
-        liked = True
+        if not created:
+            like.delete()
+            liked = False
+        else:
+            liked = True
 
-    return JsonResponse(
-        {
-            "liked": liked,
-            "like_count": post.likes.count(),  # related_name="likes" 활용
-        }
-    )
+        return JsonResponse(
+            {
+                "liked": liked,
+                "like_count": post.likes.count(),
+            }
+        )
 
 
 class CommentCreateView(LoginRequiredMixin, CreateView):
@@ -137,7 +140,36 @@ class CommentCreateView(LoginRequiredMixin, CreateView):
         post = get_object_or_404(Post, id=self.kwargs["post_id"])
         form.instance.post = post
         form.instance.user = self.request.user
+        
+        parent = form.cleaned_data.get("parent_comment")
+        if parent:
+            if parent.parent_comment:
+                form.instance.parent_comment = parent.parent_comment  # 최상위로 보정
+            else:
+                form.instance.parent_comment = parent
+
         return super().form_valid(form)
 
     def get_success_url(self):
         return reverse_lazy("post_detail", kwargs={"pk": self.kwargs["post_id"]})
+
+
+class CommentLikeToggleView(LoginRequiredMixin, View):
+    def post(self, request, comment_id):
+        comment = get_object_or_404(Comment, id=comment_id)
+        user = request.user
+
+        like, created = CommentLike.objects.get_or_create(user=user, comment=comment)
+
+        if not created:
+            like.delete()
+            liked = False
+        else:
+            liked = True
+
+        return JsonResponse(
+            {
+                "liked": liked,
+                "like_count": comment.likes.count(),
+            }
+        )
